78116b53241264503e1cb48bb1e04e1be35877b7
Muhammad Talha <talhaz5518@gmail.com>
2025-08-18 22:49:10 +0500

    Vue 01 Added (Counter Button)

---
+++ 01/src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref, watch, watchEffect } from 'vue';
+
+const count = ref(0);
+
+</script>
+
+<template>
+  <button class="btn" @click="count++">
+    The Count is {{ count }}
+  </button>
+</template>
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.btn{
+  height: 5vmax;
+  width: 15vmax;
+  cursor: pointer;
+  font-size: 1.1vmax;
+}
+</style>
